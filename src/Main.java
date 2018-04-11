749a7c7ee0f46d4134408bcf6edd2fd63e0d72b1
Unknown <benevedeselias@gmail.com>
2018-04-10 17:55:03 -0700

    Networked read/write functions
    That was easier than expected

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
+import com.jcraft.jsch.*;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Bounds;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main extends Application
 {
     Stage window;
     private Text textHolder = new Text();
-    private double oldHeight = 0;
+    private TextArea textArea = new TextArea();
+    private VBox vBox = new VBox();
+    private HBox hBox = new HBox();
+
+    private NetworkFileManager fileManager = new NetworkFileManager();
 
     @Override
     public void start(Stage primaryStage) throws Exception
@@ -20,36 +32,73 @@ public class Main extends Application
         window = primaryStage;
         window.setTitle("Minimal Auto Editor");
 
-        TextArea textArea = new TextArea();
         textArea.setPromptText("Edit your path here nerd");
 
         textArea.setPrefSize(200, 500);
         textArea.setWrapText(true);
 
-        VBox vBox = new VBox();
-
         vBox.setMinWidth(500);
         vBox.setMinHeight(500);
 
+        hBox.setMinWidth(vBox.getMinWidth());
+
+        Button readButton = new Button("Read");
+        readButton.setOnAction(e -> readButtonClicked());
+
+        Button writeButton = new Button("Write");
+        writeButton.setOnAction(e -> writeButtonClicked());
+
+        hBox.setPadding(new Insets(10));
+        hBox.setSpacing(10);
+        hBox.getChildren().addAll(readButton, writeButton);
+
         textHolder.textProperty().bind(textArea.textProperty());
-        textHolder.layoutBoundsProperty().addListener(new ChangeListener<Bounds>()
-        {
-            @Override
-            public void changed(ObservableValue<? extends Bounds> observable, Bounds oldValue, Bounds newValue)
-            {
-                if (oldHeight != newValue.getHeight())
-                {
-                    oldHeight = newValue.getHeight();
-                    textArea.setPrefHeight(vBox.getHeight() + 20); // +20 is for paddings
-                }
-            }
-        });
 
 
-        vBox.getChildren().add(textArea);
+        vBox.getChildren().addAll(textArea, hBox);
 
         Scene scene = new Scene(vBox);
         window.setScene(scene);
+
+        window.heightProperty().addListener(this::updateHeight);
+
         window.show();
+    }
+
+    public void updateHeight(ObservableValue<? extends Number> observable, Number oldHeight, Number newHeight)
+    {
+        textArea.setPrefHeight(vBox.getHeight() + 20);
+    }
+
+    public void writeButtonClicked()
+    {
+        try (PrintWriter out = new PrintWriter("output.txt")) {
+            out.println(textArea.getText());
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File not found!");
+        }
+
+        writeFile();
+    }
+
+    public void readButtonClicked()
+    {
+        fileManager.recvFile("input.txt", "/U/testOutput.txt");
+
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get("input.txt"));
+            textArea.setText(new String(encoded, "utf-8"));
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public void writeFile()
+    {
+        fileManager.sendFile("output.txt", "/U/testOutput.txt");
     }
 }
---
+++ src/NetworkFileManager.java
@@ -0,0 +1,250 @@
+/* -*-mode:java; c-basic-offset:2; indent-tabs-mode:nil -*- */
+/**
+ * This program will demonstrate the file transfer from local to remote.
+ *   $ CLASSPATH=.:../build javac ScpTo.java
+ *   $ CLASSPATH=.:../build java ScpTo file1 user@remotehost:file2
+ * You will be asked passwd. 
+ * If everything works fine, a local file 'file1' will copied to
+ * 'file2' on 'remotehost'.
+ *
+ */
+import com.jcraft.jsch.*;
+import java.io.*;
+
+
+//This is almost 100% blackbox code. Its black magic
+public class NetworkFileManager {
+    public NetworkFileManager()
+    {
+
+    }
+
+    public static void sendFile(String localFile, String remoteFile){
+        /*if(arg.length!=2){
+            System.err.println("usage: java ScpTo file1 user@remotehost:file2");
+            System.exit(-1);
+        }*/
+
+        FileInputStream fis=null;
+        try{
+
+            String lfile = localFile;
+            String user = "admin";
+            String host = "10.19.83.2";
+            String rfile = remoteFile;
+
+            JSch jsch = new JSch();
+            Session session=jsch.getSession(user, host, 22);
+
+            // username and password will be given via UserInfo interface.
+            //UserInfo ui=new MyUserInfo();
+
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.setPassword("");
+            session.connect();
+
+            boolean ptimestamp = true;
+
+            // exec 'scp -t rfile' remotely
+            String command="scp " + (ptimestamp ? "-p" :"") +" -t "+rfile;
+            Channel channel=session.openChannel("exec");
+            ((ChannelExec)channel).setCommand(command);
+
+            // get I/O streams for remote scp
+            OutputStream out=channel.getOutputStream();
+            InputStream in=channel.getInputStream();
+
+            channel.connect();
+
+            if(checkAck(in)!=0){
+                System.exit(0);
+            }
+
+            File _lfile = new File(lfile);
+
+            if(ptimestamp){
+                command="T"+(_lfile.lastModified()/1000)+" 0";
+                // The access time should be sent here,
+                // but it is not accessible with JavaAPI ;-<
+                command+=(" "+(_lfile.lastModified()/1000)+" 0\n");
+                out.write(command.getBytes()); out.flush();
+                if(checkAck(in)!=0){
+                    System.exit(0);
+                }
+            }
+
+            // send "C0644 filesize filename", where filename should not include '/'
+            long filesize=_lfile.length();
+            command="C0644 "+filesize+" ";
+            if(lfile.lastIndexOf('/')>0){
+                command+=lfile.substring(lfile.lastIndexOf('/')+1);
+            }
+            else{
+                command+=lfile;
+            }
+            command+="\n";
+            out.write(command.getBytes()); out.flush();
+            if(checkAck(in)!=0){
+                System.exit(0);
+            }
+
+            // send a content of lfile
+            fis=new FileInputStream(lfile);
+            byte[] buf=new byte[1024];
+            while(true){
+                int len=fis.read(buf, 0, buf.length);
+                if(len<=0) break;
+                out.write(buf, 0, len); //out.flush();
+            }
+            fis.close();
+            fis=null;
+            // send '\0'
+            buf[0]=0; out.write(buf, 0, 1); out.flush();
+            if(checkAck(in)!=0){
+                System.exit(0);
+            }
+            out.close();
+
+            channel.disconnect();
+            session.disconnect();
+        }
+        catch(Exception e){
+            System.out.println(e);
+            try{if(fis!=null)fis.close();}catch(Exception ee){}
+        }
+    }
+
+    public void recvFile(String localFile, String remoteFile){
+
+        FileOutputStream fos=null;
+        try{
+
+            String user = "admin";
+            String host = "10.19.83.2";
+            String rfile = remoteFile;
+            String lfile = localFile;
+
+            String prefix=null;
+            if(new File(lfile).isDirectory()){
+                prefix=lfile+File.separator;
+            }
+
+            JSch jsch=new JSch();
+            Session session=jsch.getSession(user, host, 22);
+
+            // username and password will be given via UserInfo interface.
+
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.setPassword("");
+            session.connect();
+
+            // exec 'scp -f rfile' remotely
+            String command="scp -f "+rfile;
+            Channel channel=session.openChannel("exec");
+            ((ChannelExec)channel).setCommand(command);
+
+            // get I/O streams for remote scp
+            OutputStream out=channel.getOutputStream();
+            InputStream in=channel.getInputStream();
+
+            channel.connect();
+
+            byte[] buf=new byte[1024];
+
+            // send '\0'
+            buf[0]=0; out.write(buf, 0, 1); out.flush();
+
+            while(true){
+                int c=checkAck(in);
+                if(c!='C'){
+                    break;
+                }
+
+                // read '0644 '
+                in.read(buf, 0, 5);
+
+                long filesize=0L;
+                while(true){
+                    if(in.read(buf, 0, 1)<0){
+                        // error
+                        break;
+                    }
+                    if(buf[0]==' ')break;
+                    filesize=filesize*10L+(long)(buf[0]-'0');
+                }
+
+                String file=null;
+                for(int i=0;;i++){
+                    in.read(buf, i, 1);
+                    if(buf[i]==(byte)0x0a){
+                        file=new String(buf, 0, i);
+                        break;
+                    }
+                }
+
+                //System.out.println("filesize="+filesize+", file="+file);
+
+                // send '\0'
+                buf[0]=0; out.write(buf, 0, 1); out.flush();
+
+                // read a content of lfile
+                fos=new FileOutputStream(prefix==null ? lfile : prefix+file);
+                int foo;
+                while(true){
+                    if(buf.length<filesize) foo=buf.length;
+                    else foo=(int)filesize;
+                    foo=in.read(buf, 0, foo);
+                    if(foo<0){
+                        // error
+                        break;
+                    }
+                    fos.write(buf, 0, foo);
+                    filesize-=foo;
+                    if(filesize==0L) break;
+                }
+                fos.close();
+                fos=null;
+
+                if(checkAck(in)!=0){
+                    System.exit(0);
+                }
+
+                // send '\0'
+                buf[0]=0; out.write(buf, 0, 1); out.flush();
+            }
+
+            session.disconnect();
+        }
+        catch(Exception e){
+            System.out.println(e);
+            try{if(fos!=null)fos.close();}catch(Exception ee){}
+        }
+    }
+
+    static int checkAck(InputStream in) throws IOException{
+        int b=in.read();
+        // b may be 0 for success,
+        //          1 for error,
+        //          2 for fatal error,
+        //          -1
+        if(b==0) return b;
+        if(b==-1) return b;
+
+        if(b==1 || b==2){
+            StringBuffer sb=new StringBuffer();
+            int c;
+            do {
+                c=in.read();
+                sb.append((char)c);
+            }
+            while(c!='\n');
+            if(b==1){ // error
+                System.out.print(sb.toString());
+            }
+            if(b==2){ // fatal error
+                System.out.print(sb.toString());
+            }
+        }
+        return b;
+    }
+}
